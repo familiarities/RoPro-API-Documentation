3beaa5f28f83184377487839fd11ea1ff70b1456
0x8A0F <124242457+familiarities@users.noreply.github.com>
2024-06-21 09:29:50 +0300

    get item comments

---
+++ Documentation/Comments/getComments.php
@@ -0,0 +1,36 @@
+## RoPro - Comments - Get Comments
+
+URL: https://api.ropro.io/getComments.php \
+Method: POST \
+Auth Required: No
+
+## Query Parameters
+
+`id` - integer - Item ID the comments are for\
+`page` - string - What page of comments you want to fetch
+
+---
+
+_Example Response_
+
+```json
+{
+  "comments": [
+    {
+      "username": "Roblox",
+      "userid": "1",
+      "comment": "these ropro api docs are amazing",
+      "date": "2022-10-08 11:40:06.764119"
+    },
+    {
+      "username": "Builderman",
+      "userid": "156",
+      "comment": "i know right we should 100% donate to the author",
+      "date": "2022-10-08 11:41:10.795055"
+    }
+  ],
+  "more": false, 
+  "page": 0, 
+  "valid": true
+}
+```
